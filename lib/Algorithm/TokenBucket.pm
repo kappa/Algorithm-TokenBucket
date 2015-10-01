4c91e1100814c3b8d400530995c404bba57c874d
Alex Kapranoff <alex@kapranoff.ru>
2015-10-01 19:13:27 +0300

    Minilla conversion
    + some wording clean up
    + move to GPL3

--- lib/Algorithm/TokenBucket.pm
+++ lib/Algorithm/TokenBucket.pm
@@ -2,14 +2,12 @@ package Algorithm::TokenBucket;
 
 use 5.006;
 
-our $VERSION = 0.36;
+our $VERSION = 0.37;
 
 use warnings;
 use strict;
 
-BEGIN {
-    eval { require Time::HiRes; import Time::HiRes 'time' } # use if available
-}
+use Time::HiRes;
 
 =head1 NAME
 
@@ -23,21 +21,21 @@ Algorithm::TokenBucket - Token bucket rate limiting algorithm
     # with bursts of 5 items max
     my $bucket = new Algorithm::TokenBucket 100 / 3600, 5;
 
-    # wait till we're allowed to process 3 items
+    # wait until we are allowed to process 3 items
     until ($bucket->conform(3)) {
         sleep 0.1;
         # do things
     }
-    
+ 
     # process 3 items because we now can
     process(3);
 
     # leak (flush) bucket
-    $bucket->count(3);  # or, e.g. $bucket->count(1) for 1..3;
+    $bucket->count(3);  # same as $bucket->count(1) for 1..3;
 
     if ($bucket->conform(10)) {
-        die for 'truth';
-        # because the bucket with a burst size of 5
+        die;
+        # because a bucket with the burst size of 5
         # will never conform to 10
     }
 
@@ -49,7 +47,7 @@ Algorithm::TokenBucket - Token bucket rate limiting algorithm
             $bucket->count(5);
         }
     }
-    # we're likely to have processed 200 items (and hogged CPU, btw)
+    # we're likely to have processed 200 items (and hogged CPU)
 
     Storable::store $bucket, 'bucket.stored';
     my $bucket1 = new Algorithm::TokenBucket
@@ -57,18 +55,17 @@ Algorithm::TokenBucket - Token bucket rate limiting algorithm
 
 =head1 DESCRIPTION
 
-Token bucket algorithm is a flexible way of imposing a rate limit
+Token Bucket algorithm is a flexible way of imposing a rate limit
 against a stream of items. It is also very easy to combine several
 rate-limiters in an C<AND> or C<OR> fashion.
 
-Each bucket has a memory footprint of constant size because the
-algorithm is based on C<information rate>. This was my main motivation to
-implement it. Other rate limiters on CPAN keep track of I<ALL> incoming
-items in memory. It allows them to be precisely accurate.
+Each bucket has a constant memory footprint because the
+algorithm is based on C<information rate>.
+Other rate limiters available on CPAN keep track of I<ALL> incoming
+items in memory. It allows them to be much more accurate.
 
 FYI, C<conform>, C<count>, C<information rate>, C<burst size> terms are
-shamelessly borrowed from http://linux-ip.net/gl/tcng/node62.html
-page.
+taken from L<http://linux-ip.ntaгрgl/tcng/node62.html> page.
 
 =head1 INTERFACE
 
@@ -84,8 +81,8 @@ use fields qw/info_rate burst_size _tokens _last_check_time/;
 
 The constructor takes as parameters at least C<rate of information> in
 items per second and C<burst size> in items. It can also take current
-token counter and last check time but this usage is reserved for
-restoring a saved bucket, beware. See L</state>.
+token counter and last check time but this usage is mostly intended for
+restoring a saved bucket. See L</state>.
 
 =cut
 
@@ -143,12 +140,12 @@ sub _token_flow {
 
 =item conform($)
 
-This sub checks if the bucket contains at least I<N> tokens. In that
-case it is allowed to transmit (or process) I<N> items (not
+This method checks if the bucket contains at least I<N> tokens. In that
+case it is allowed to transmit or process I<N> items (not
 exactly right because I<N> can be fractional) from the stream. A bucket never
 conforms to an I<N> greater than C<burst size>.
 
-It returns a boolean value.
+The method returns a boolean value.
 
 =cut
 
@@ -163,7 +160,7 @@ sub conform {
 
 =item count($)
 
-This sub removes I<N> (or all if there are less than I<N> available) tokens from the bucket.
+This method removes I<N> (or all if there are less than I<N> available) tokens from the bucket.
 Does not return a meaningful value.
 
 =cut
@@ -179,13 +176,13 @@ sub count {
 
 =item until($)
 
-This sub returns the number of seconds until I<N> tokens can be removed from the bucket.
-It's especially useful in multitasking environments like L<POE> where you
+This method returns the number of seconds until I<N> tokens can be removed from the bucket.
+It is especially useful in multitasking environments like L<POE> where you
 cannot busy-wait. One can safely schedule the next conform($N) check in until($N)
 seconds instead of checking repeatedly.
 
-Note that until() does not take into account C<burst size>. That means
-a bucket will not conform to I<N> even after sleeping for until($N)
+Note that until() does not take into account C<burst size>. This means
+that a bucket will not conform to I<N> even after sleeping for until($N)
 seconds if I<N> is greater than C<burst size>.
 
 =cut
@@ -211,7 +208,7 @@ Returns the current number of tokens in the bucket. This method may be
 useful for inspection or debugging purposes. You should not examine
 the state of the bucket for rate limiting purposes.
 
-This number will frequently be fractional so it's not exactly a
+This number will frequently be fractional so it is not exactly a
 "count".
 
 =cut
@@ -229,9 +226,8 @@ __END__
 
 =head1 EXAMPLES
 
-Think a rate limiter for a mail sending application. We'd like to
+Imagine a rate limiter for a mail sending application. We would like to
 allow 2 mails per minute but no more than 20 mails per hour.
-Go, go, go!
 
     my $rl1 = new Algorithm::TokenBucket 2/60, 1;
     my $rl2 = new Algorithm::TokenBucket 20/3600, 10;
@@ -264,10 +260,8 @@ L</until()> method.
 
 =head1 BUGS
 
-Works unreliably for fractional rates unless Time::HiRes is present.
-
 Documentation lacks the actual algorithm description. See links or read
-the source (there are about 20 lines of sparse perl in several subs, trust me).
+the source (there are about 20 lines of sparse perl in several subs).
 
 until($N) does not return infinity if $N is greater than C<burst
 size>. Sleeping for infinity seconds is both useless and hard to
@@ -282,19 +276,19 @@ Alexey Shrub contributed the L</get_token_count> method.
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (C) 2012 by Alex Kapranoff.
+This software is copyright (C) 2015 by Alex Kapranoff.
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
+the terms GNU General Public License version 3.
 
 =head1 AUTHOR
 
-Alex Kapranoff, E<lt>kappa@cpan.orgE<gt>
+Alex Kapranoff, E<lt>alex@kapranoff.ru<gt>
 
 =head1 SEE ALSO
 
 http://www.eecs.harvard.edu/cs143/assignments/pa1/,
-http://en.wikipedia.org/wiki/Token_bucket, 
+http://en.wikipedia.org/wiki/Token_bucket,
 http://linux-ip.net/gl/tcng/node54.html,
 http://linux-ip.net/gl/tcng/node62.html,
 L<Schedule::RateLimit>, L<Algorithm::FloodControl>.
